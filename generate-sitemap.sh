c9121edeb256b81bf6acd85ea7c07a8324fd4d37
hirokuma <hiro9ma@gmail.com>
2025-06-28 13:22:26 +0900

    sitemap & tags

--- generate-sitemap.sh
+++ generate-sitemap.sh
@@ -1,40 +1,6 @@
 #!/bin/bash
 
-# url configuration
-URL="https://blog.hirokuma.work/"
+SITEMAP="my-sitemap.xml"
 
-# begin new sitemap
-exec 1> my-sitemap.xml
-
-# print head
-echo '<?xml version="1.0" encoding="UTF-8"?>'
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
-
-# print urls
-find . \( -type d -and -name "201*" -and -prune \) -or -type f \( -name "*.md" -o -name "*.html" \) -printf "%TY-%Tm-%Td%p\n" | \
-  sed -e "s/\.md$/\.html/g" | \
-  while read -r line; do
-    DATE=${line:0:10}
-    FILE=${line:12}
-    if [ ${FILE:0:1} == "_" ]; then
-      continue
-    fi
-    if [[ $FILE =~ ^201 ]]; then
-      continue
-    fi
-    if [[ $FILE =~ ^202[0-3] ]]; then
-      continue
-    fi
-    if [[ $FILE =~ ^2024/0[1-3] ]]; then
-      continue
-    fi
-    echo "<url>"
-    echo "<loc>${URL}${FILE}</loc>"
-    echo "<lastmod>${DATE}T00:00:00+09:00</lastmod>"
-    echo "</url>"
-  done
-
-# print foot
-echo "</urlset>"
-
-# cp my-sitemap.xml sitemap.xml
+rm -f $SITEMAP
+curl --silent -o $SITEMAP https://blog.hirokuma.work/sitemap.xml
--- matome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -u
-
-if [ ! -f $1/index.md ]; then
-    echo \"$1/index.md\" not found
-    return
-fi
-
-sed -i -e "s|($1/|(/$1/|g" -e "s/\.md/\.html/g" $1/index.md
